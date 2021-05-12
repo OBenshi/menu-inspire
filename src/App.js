bcae3114b9d402a0da577fcadb5ce7bc84821659
OBenshi <odedbenshi@gmail.com>
2021-05-12 18:05:56 +0200

    first comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import GetMenus from "./components/GetMenus";
+import apiKey from "./key.js";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 function App() {
+  // const [menus, setMenus] = useState([]);
+  // const fetchMenus = () => {
+  // axios
+  //   .get(`http://api.menus.nypl.org/menus?`)
+  //   .then((res) => {
+  //     console.log(res);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+  // fetch(
+  // `http://www.nokeynoshade.party/api/queens/all`
+  // `http://api.menus.nypl.org/menus`,
+  // {
+  //   headers: {
+  //     Authorization: `Token token=${apiKey}`,
+  //   },
+  // }
+  // `https://ridb.recreation.gov/api/v1/campsites?offset=0`,
+  // {
+  //   headers: {
+  //     apikey: apiKey,
+  //   },
+  // }
+  // )
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     console.log(data);
+
+  //     setMenus(data);
+  //   })
+  //   .catch((e) => {
+  //     console.log(e);
+  //   });
+  // };
+  // useEffect(() => {
+  //   fetchMenus();
+  // });
+  // console.log(menus);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GetMenus />
+      {/* appbar
+      display
+      footer  */}
     </div>
   );
 }
